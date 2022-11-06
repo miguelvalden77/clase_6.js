d26f25af005c8b69d0e41c7f8c123a89c317ff3b
miguelvalden <miguelvalden77@gmail.com>
2022-11-06 23:00:43 +0100

    clase 6

---
+++ javascript.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+// Ejercicios
+
+    // sort
+    // 1. Haz una función que reciba un array de números como parámetro. La función debe devolver dicho array pero ordenado de mayor a menor
+
+
+    // 2. Tomando la misma función que el ejercicio anterior, haz que la función reciba dos parámetros: El mismo array de números y un string que puede ser "down" si se quiere ordenar el array de menor a mayor y "up" de mayor a menor
+
+
+    // 3. Haz una función que reciba como parámetro un array de strings y tiene que devolver dicho array ordenado alfabéticamente
+
+
+    // 4. Haz una función que reciba como parámetro un array de objetos y tiene que devolver dicho array ordenado por la calificación numérica de mayor a menor
+
+    let alumnos = [
+        {
+            nombre: "Miguel",
+            nota: 6
+        },
+        {
+            nombre: "Lucía",
+            nota: 8
+        },
+        {
+            nombre: "Luis",
+            nota: 7.5
+        },
+        {
+            nombre: "Juanpe",
+            nota: 10
+        }
+    ]
+
+
+    // foreach
+
+    // 1. Dado un array, saluda a todos añadiendo Hola a su nombre. NO ES UNA FUNCIÓN
+    let amigos = ["Marcos", "Marta", "Alba", "Lucia", "Carlos"]
+
+
+    // 2. Haz una función que reciba como parámetro un array de números y lo devuelva multiplicando los numeros por 2
+
+
+    // 3. Tomando la función anterior, ahora haz que si los números son impares los multiplique por 2 y si son pares los divida entre 2.
